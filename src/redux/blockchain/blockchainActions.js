580cf2b107033519c144fa9dd23f8281357533a3
Ben Luitgaarden <bluitgaarden@Bens-MacBook-Pro.local>
2021-10-13 15:27:30 -0400

    updated blockchainActions

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -1,7 +1,7 @@
 // constants
 import Web3EthContract from "web3-eth-contract";
 import Web3 from "web3";
-import SmartContract from "../../contracts/goats66.json";
+import SmartContract from "../../contracts/goatTroopNFT.json";
 // log
 import { fetchData } from "../data/dataActions";
 
